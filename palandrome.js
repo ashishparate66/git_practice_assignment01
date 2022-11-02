05b49c8626c131894b6784b9d92ff0f72b1d0231
Snehal-arsad <snehalarsad52@gmail.com>
2022-11-02 13:52:24 +0530

    Am fixed this code for git assignment

--- palandrome.js
+++ palandrome.js
@@ -3,7 +3,7 @@ function CheckPalindrome(string){
   for(let i=string.length-1;i>=0;i--){
     bag=bag+string[i];
   }
-  if(string!==bag){
+  if(string===bag){
     return true;
   }
   return false;
--- prime.js
+++ prime.js
@@ -2,7 +2,7 @@ function CheckPrime(number){
   let count=0;
   for(let i=1;i<=number;i++){
     if(number%i==0){
-         count;
+         count++;
     }
   }
   if(count===2){
