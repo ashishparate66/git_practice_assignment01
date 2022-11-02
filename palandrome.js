90210b82c6cdc1e878095d477516bc080df037a2
unknown <ashishparate66@gmail.com>
2022-11-02 13:38:54 +0530

    add prime and palandrome code

---
+++ palandrome.js
@@ -0,0 +1,18 @@
+let bag="";
+function CheckPalindrome(string){
+  for(let i=string.length-1;i>=0;i--){
+    bag=bag+string[i];
+  }
+  if(string!==bag){
+    return true;
+  }
+  return false;
+}
+
+let ans=CheckPalindrome("naman")
+if(ans==true){
+  console.log("Its a palindrome");
+}
+else{
+  console.log("not a palindrome");
+}
---
+++ prime.js
@@ -0,0 +1,20 @@
+function CheckPrime(number){
+  let count=0;
+  for(let i=1;i<=number;i++){
+    if(number%i==0){
+         count;
+    }
+  }
+  if(count===2){
+   return true;
+     }
+  return false;
+}
+let ans=CheckPrime(13); 
+if(ans == true){
+  console.log("Its a prime number");
+}
+else{
+  console.log("Not a prime number");
+}
+
